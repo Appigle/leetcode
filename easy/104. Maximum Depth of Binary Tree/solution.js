b8372a3ea63e20bf170ae05793ae29aa8b900c77
Ray Chen <ray.chenley.up@gmail.com>
2025-06-27 17:51:06 -0400

    feat: 104. Maximum Depth of Binary Tree

---
+++ easy/104. Maximum Depth of Binary Tree/solution.js
@@ -0,0 +1,36 @@
+const maxDepth = (root) => {
+  if (!root) return 0;
+  return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+};
+
+maxDepth([3, 9, 20, null, null, 15, 7]);
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth2 = function (root) {
+  if (!root) return 0;
+  const queue = [root];
+  let depth = 0;
+  while (queue.length > 0) {
+    const levelSize = queue.length;
+    for (let i = 0; i < levelSize; i++) {
+      const node = queue.shift();
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+    depth++;
+  }
+  return depth;
+};
+
+maxDepth2([3, 9, 20, null, null, 15, 7]);
